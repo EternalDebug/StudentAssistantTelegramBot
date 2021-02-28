dbd1743f8f65233bfbd197ae6f0ca617d4eb19ce
EternalDebug <71968876+EternalDebug@users.noreply.github.com>
2021-02-28 17:54:09 +0300

    Update Shedule_Sender.cs

--- StudentAssistantTelegramBot/Shedule_Sender.cs
+++ StudentAssistantTelegramBot/Shedule_Sender.cs
@@ -6,95 +6,119 @@ namespace StudentAssistantTelegramBot
 {
     class Shedule_Sender
     {
-        static int test_min = 8;
-        static int test_hour = 20;
+        static int test_min = 13;
+        static int test_hour = 16;
+
+        public class SendObj//Класс объекта отправки оповещения.
+        {
+            public DateTime SendTime;
+            public long uid;
+            public string name;
+
+            public SendObj(DateTime DT, long id, string namen)
+            {
+                SendTime = DT;
+                uid = id;
+                name = namen;
+            }
+        }
         
         public static void Bot_SendWarn()
         {
-            Dictionary<long, string> NeedSend = new Dictionary<long, string>();//Нужно будет поменять bool на string с названием дисциплины. Чтоб избежать коллизии можно попробовать в стеки
+            //Dictionary<long, string> NeedSend = new Dictionary<long, string>();
+            List<SendObj> NeedSend = new List<SendObj>();
             while (true)
             {
                 DateTime now = DateTime.Now;
-                if ((now.Hour == 8 && now.Minute == 0 && now.Second == 0) || (now.Hour == 12 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == 16 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == 20 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == test_hour && now.Minute == test_min && now.Second == 0))
+                //if ((now.Hour == 8 && now.Minute == 0 && now.Second == 0) || (now.Hour == 12 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == 16 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == 20 && now.Minute == 0 && now.Second == 0 ) || (now.Hour == test_hour && now.Minute == test_min && now.Second == 0))
+                if (now.Minute % 2 == 0 && now.Second == 0 && now.Millisecond == 0  || (now.Hour == test_hour && now.Minute == test_min && now.Second == 0))
                 {
                     NeedSend = CreateNotSendedArr();
                 }
 
-                if ((now.Hour == 8 && now.Minute == 0 && now.Second == 3 && now.Millisecond == 0) || (now.Hour == 12 && now.Minute == 0 && now.Second == 3 && now.Millisecond == 0) || (now.Hour == 16 && now.Minute == 0 && now.Second == 3 && now.Millisecond == 0) || (now.Hour == 20 && now.Minute == 0 && now.Second == 3 && now.Millisecond == 0) || (now.Hour == test_hour && now.Minute == test_min && now.Second == 3 && now.Millisecond == 0))
-                {
-                    Bot_SendWarn_core(now, ref NeedSend);
-                }
+                
+                Bot_SendWarn_core(now, ref NeedSend);
+                
             }
         }
 
 
 
-        public static void Bot_SendWarn_core(DateTime now, ref Dictionary<long, string> NeedSend)
+        public static void Bot_SendWarn_core(DateTime now, ref List<SendObj> NeedSend)
         {
-            foreach (var i in NeedSend.Keys)
+            foreach (var i in NeedSend)
             {
-                
-                    string answer = $"Время подготовки. Сейчас у вас {NeedSend[i]}.";
+                if (now.Hour == i.SendTime.Hour && now.Minute == i.SendTime.Minute && now.Second == i.SendTime.Second && now.Millisecond == i.SendTime.Millisecond)
+                {
+                    string answer = $"Время подготовки. Сейчас у вас {i.name}.";
                     Student st;
-                    if (Program.students.ContainsStudByID(i,out st))
+                    if (Program.students.ContainsStudByID(i.uid, out st))
                     {
                         st.prev_loc = st.users_loc;
                         st.users_loc = LevelOfCode.Question_1;
                         answer += " Вы планируете сейчас готовиться?";//да или нет
-                        //Место для переключения кода доступа. Внедрить диалог
-                        
                     }
-                    
-                    Program.Bot.SendTextMessageAsync(i, answer);
-                    Console.WriteLine($"Оповещение отправлено юзеру {i}");
-                    //NeedSend[i] = false; //Создаёт коллизию в потоке. Не трогать под страхом расстрела через повешание
-                
 
+                    Program.Bot.SendTextMessageAsync(i.uid, answer);
+                    Console.WriteLine($"Оповещение отправлено юзеру {i.uid}");
+                }
             }
             
+        }
+
+        public static bool MyDateLessNow(DateTime DT)
+        {
+            bool res = false;
+            DateTime Now = DateTime.Now;
+            if (DT.Hour < Now.Hour)
+                return true;
+            else if (DT.Hour == Now.Hour)
+                if (DT.Minute < Now.Minute)
+                    return true;
+                else if (DT.Minute == Now.Minute)
+                    if (DT.Second < Now.Second-1)
+                        return true;
+                    else if (DT.Second == Now.Second)
+                        if (DT.Millisecond < Now.Millisecond)
+                            return true;
+
+                        return res;
         }
 
         //tester id 790754149
 
 
         /// <summary>
-        /// Содержит ли массив дат сегодня
+        /// Фильтр сегодняшнего числа
         /// </summary>
         /// <param name="Shed"></param>
         /// <param name="today"></param>
         /// <returns></returns>
-        public static bool ContData(DateTime[] Shed)
+        public static bool ContData(DateTime Shed)
         {
-            bool res = false;
             DateTime now = DateTime.Now;
-            for (int i =0; i< Shed.Length;i++)
-            {
-                if (Shed[i].Date == now.Date && Shed[i].Hour == now.Hour && Shed[i].Minute == now.Minute)
-                    return true;
-            }
-            return res;
+            //return (Shed.Date == now.Date && Shed.Hour == now.Hour && Shed.Minute == now.Minute);
+            return (Shed.Date == now.Date);
         }
 
 
-        public static Dictionary<long,string> CreateNotSendedArr()
+        public static List<SendObj> CreateNotSendedArr()
         {
-            Dictionary<long, string> res = new Dictionary<long, string>();
+            //Dictionary<long, string> res = new Dictionary<long, string>();
+            List<SendObj> res = new List<SendObj>();
             foreach (var i in Program.students.list)
             {
-                if (i.Shedule.Keys.Count != 0)
-                {
-                    foreach (var k in i.Shedule.Keys)
+                 foreach (var k in i.Shedule.Keys)
+                 {
+                    foreach (var j in i.Shedule[k])
                     {
-
-                        if (ContData(i.Shedule[k]))//Заменить Today на какое-то время. Предположительно 10.00. А лучше реализовать проверку
-                        {
-                            res.Add(i.student_id, k);
-                        }
+                        if (ContData(j) && !MyDateLessNow(j))
+                            res.Add(new SendObj(j, i.student_id, k));
                     }
-                }
+                 }
             }
 
-            return res;
+             return res;
         }
 
         public static void SetTestShed(ref Students s, long uid)
@@ -103,8 +127,13 @@ namespace StudentAssistantTelegramBot
             for (int i = 0; i< s.list.Count;i++)
             {
                 DateTime td = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, test_hour, test_min, 0);
+                DateTime td2 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, test_hour, test_min+3, 0);
+                DateTime td3 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, test_hour+1, test_min + 3, 0);
                 if (s.list[i].student_id == uid)
-                    s.list[i].Shedule.Add("Test shedule", new DateTime[] { td });
+                {
+                    s.list[i].Shedule.Add("Test shedule", new DateTime[] { td,td2,td3 });
+                }
+
             }
         }
 
